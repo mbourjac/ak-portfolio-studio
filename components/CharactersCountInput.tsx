3537c235e3807d40f12e8ffd81aca71d1d78aca2
Michael Bourjac <michael.bourjac@gmail.com>
2023-10-24 19:13:16 +0200

    Add optional threshold prop to CharactersCountInput

--- components/CharactersCountInput.tsx
+++ components/CharactersCountInput.tsx
@@ -6,6 +6,7 @@ type CharactersCountInputProps = StringInputProps<StringSchemaType> & {
   charactersCount: number;
   isTextArea?: boolean;
   message?: string;
+  threshold?: number;
 };
 
 export const CharactersCountInput = ({
@@ -15,6 +16,7 @@ export const CharactersCountInput = ({
   charactersCount,
   isTextArea,
   message,
+  threshold = 0,
 }: CharactersCountInputProps) => {
   const handleChange = useCallback(
     (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -24,11 +26,15 @@ export const CharactersCountInput = ({
     [onChange]
   );
 
+  const baseColor = '#6e7683';
+  const cautionColor = '#e7b453';
+  const warningColor = '#f03e2f';
+
   return (
     <Stack space={2}>
       <Text
         style={{
-          color: '#6e7683',
+          color: baseColor,
           fontSize: '0.8125rem',
           paddingBottom: '0.5rem',
         }}
@@ -41,7 +47,12 @@ export const CharactersCountInput = ({
         )}
         <span
           style={{
-            color: value.length > charactersCount ? '#f03e2f' : '#6e7683',
+            color:
+              value.length < charactersCount
+                ? baseColor
+                : value.length < charactersCount + threshold
+                ? cautionColor
+                : warningColor,
           }}
         >
           Character count: {value.length}.
